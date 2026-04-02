1b846bd229f015a6beee9715eddd33ef10aa6f6b
allgemeiner-intellekt <mail.yuhan.li@gmail.com>
2026-04-02 21:16:08 +0100

    feat: wire up PapyroApp with onboarding flow and library detection

--- Papyro/PapyroApp.swift
+++ Papyro/PapyroApp.swift
@@ -2,9 +2,26 @@ import SwiftUI
 
 @main
 struct PapyroApp: App {
+    @State private var appState = AppState()
+    @State private var libraryManager: LibraryManager?
+
     var body: some Scene {
         WindowGroup {
-            Text("Papyro")
+            Group {
+                if appState.isOnboarding {
+                    WelcomeView()
+                } else {
+                    MainView()
+                }
+            }
+            .environment(appState)
+            .environment(libraryManager ?? LibraryManager(appState: appState))
+            .onAppear {
+                if libraryManager == nil {
+                    libraryManager = LibraryManager(appState: appState)
+                }
+                libraryManager?.detectExistingLibrary()
+            }
         }
     }
 }
